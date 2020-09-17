fb3cff81583be15c6682fb50febe1aa6741c292a
Ashutosh Kumar <ashutoshkmr40@gmail.com>
2020-09-17 07:58:46 +0530

    add socket io

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const path = require("path")
+const http = require("http");
+const socketio = require("socket.io")
+const publicDir = path.resolve(__dirname, "public")
+
 
 const app = express();
+app.use(express.static(publicDir));
+
+const server = http.createServer(app)
+const io = socketio(server);
+
+io.on('connection', socket => {
+    console.log("New WS connection");
+
+    socket.emit("message", "Sup client")
+})
+
 
 const PORT = 3000;
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
